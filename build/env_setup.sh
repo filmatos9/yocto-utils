f230a7b1e80eff08d0eb91011e6700bfaf1db188
Filipe Matos <filmatos9@gmail.com>
2023-09-30 16:10:45 -0400

    Update env var names

--- build/env_setup.sh
+++ build/env_setup.sh
@@ -1,27 +1,28 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export FILIPE_BUILD_DIR="$SCRIPT_DIR/build"
-export FILIPE_SSTATE_DIR="$HOME/yocto/kirkstone/sstate-cache"
-export FILIPE_DOWNLOADS_DIR="$HOME/yocto/kirkstone/downloads"
+export ENV_VAR_BUILD_DIR="$SCRIPT_DIR/build"
+export ENV_VAR_SSTATE_DIR="$HOME/yocto/kirkstone/sstate-cache"
+export ENV_VAR_DOWNLOADS_DIR="$HOME/yocto/kirkstone/downloads"
 export TEMPLATECONF="$SCRIPT_DIR/yocto/meta-filipe/conf"
 export MACHINE="qemux86-64"
 
-export SSTATE_DIR="$FILIPE_SSTATE_DIR"
-export DL_DIR="$FILIPE_DOWNLOADS_DIR"
+# Delete old local.conf and bblayers.conf 
+rm $ENV_VAR_BUILD_DIR/conf/local.conf
+rm $ENV_VAR_BUILD_DIR/conf/bblayers.conf
 
-source "$SCRIPT_DIR/yocto/poky/oe-init-build-env" "$FILIPEBUILDDIR"
-export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS SSTATE_DIR DL_DIR"
+source "$SCRIPT_DIR/yocto/poky/oe-init-build-env" "$ENV_VAR_BUILD_DIR"
+export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS ENV_VAR_SSTATE_DIR ENV_VAR_DOWNLOADS_DIR"
 
 echo " "
-echo "***********************************************"
+echo "******************************************************"
 echo "Ford Configuration Settings"
-echo "***********************************************"
-echo "Template Configuration:   $TEMPLATECONF"
-echo "Build Directory:          $FILIPE_BUILD_DIR"
-echo "Downloads Directory:      $FILIPE_DOWNLOADS_DIR"
-echo "SSTATE Directory:         $FILIPE_SSTATE_DIR"
-echo "Machine:                  $MACHINE"
-echo "***********************************************"
+echo "******************************************************"
+echo "Template Configuration:           $TEMPLATECONF"
+echo "Build Directory:                  $ENV_VAR_BUILD_DIR"
+echo "Downloads Directory:              $ENV_VAR_DOWNLOADS_DIR"
+echo "Shared State Cache Directory:     $ENV_VAR_SSTATE_DIR"
+echo "Machine:                          $MACHINE"
+echo "******************************************************"
 
 # with template config, we don't need to add the layers
 # bitbake-layers add-layer ../meta-openembedded/meta-oe 
