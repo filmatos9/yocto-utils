f918718f7b18396def93219bf6a984e990b2a864
Filipe Matos <filmatos9@gmail.com>
2023-09-30 09:18:29 -0400

    Update env_setup.sh

--- build/env_setup.sh
+++ build/env_setup.sh
@@ -1,19 +1,25 @@
-#!/bin/bash
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export FILIPEBUILDDIR="$SCRIPT_DIR/../../../build"
-export TEMPLATECONF="$SCRIPT_DIR/../meta-filipe/conf"
+export FILIPE_BUILD_DIR="$SCRIPT_DIR/build"
+export FILIPE_SSTATE_DIR="$HOME/yocto/kirkstone/sstate-cache"
+export FILIPE_DOWNLOADS_DIR="$HOME/yocto/kirkstone/downloads"
+export TEMPLATECONF="$SCRIPT_DIR/yocto/meta-filipe/conf"
 export MACHINE="qemux86-64"
 
-source "$SCRIPT_DIR/../poky/oe-init-build-env" "$FILIPEBUILDDIR"
+export SSTATE_DIR="$FILIPE_SSTATE_DIR"
+export DL_DIR="$FILIPE_DOWNLOADS_DIR"
+
+source "$SCRIPT_DIR/yocto/poky/oe-init-build-env" "$FILIPEBUILDDIR"
+export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS SSTATE_DIR DL_DIR"
 
 echo " "
 echo "***********************************************"
 echo "Ford Configuration Settings"
 echo "***********************************************"
 echo "Template Configuration:   $TEMPLATECONF"
-echo "Build Directory:          $FILIPEBUILDDIR"
+echo "Build Directory:          $FILIPE_BUILD_DIR"
+echo "Downloads Directory:      $FILIPE_DOWNLOADS_DIR"
+echo "SSTATE Directory:         $FILIPE_SSTATE_DIR"
 echo "Machine:                  $MACHINE"
 echo "***********************************************"
 
